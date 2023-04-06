a02151d45b66f94f8cb13fa3b201f6bff1fae114
dietomatenpflanze <130071775+dietomatenpflanze@users.noreply.github.com>
2023-04-06 21:59:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,80 +21,78 @@ startStopBtn.addEventListener('click', () => {
 
 // Function to start text detection
 function startTextDetection() {
-
-// camera access
-checkCameraAccess()
-.then(() => {
-isDetecting = true;
-startStopBtn.textContent = 'Stop';
-feedbackText.textContent = 'Text detection started...';
-feedbackText.style.color = 'green';
-captureVideoFrame();
-})
-.catch((error) => {
-console.error(error);
-feedbackText.textContent = 'Failed to access camera. Please check camera permissions.';
-feedbackText.style.color = 'red';
-});
+    // Check camera access
+    checkCameraAccess()
+        .then(() => {
+            isDetecting = true;
+            startStopBtn.textContent = 'Stop';
+            feedbackText.textContent = 'Text detection started...';
+            feedbackText.style.color = 'green';
+            captureVideoFrame();
+        })
+        .catch((error) => {
+            console.error(error);
+            feedbackText.textContent = 'Failed to access camera. Please check camera permissions.';
+            feedbackText.style.color = 'red';
+        });
 }
 
 // Function to stop text detection
 function stopTextDetection() {
-isDetecting = false;
-startStopBtn.textContent = 'Start';
-feedbackText.textContent = 'Text detection stopped.';
-feedbackText.style.color = 'black';
-if (tesseractWorker) {
-tesseractWorker.terminate();
-tesseractWorker = null;
-}
+    isDetecting = false;
+    startStopBtn.textContent = 'Start';
+    feedbackText.textContent = 'Text detection stopped.';
+    feedbackText.style.color = 'black';
+    if (tesseractWorker) {
+        tesseractWorker.terminate();
+        tesseractWorker = null;
+    }
 }
 
 // Function to capture video frame and process for text detection
 function captureVideoFrame() {
-if (!isDetecting) {
-return;
-}
+    if (!isDetecting) {
+        return;
+    }
 
-ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-const result = Tesseract.recognize(imageData, {
-    lang: 'eng',
-    tessedit_char_whitelist: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,!?@#$%&*()-+=/:;"\''
-});
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const result = Tesseract.recognize(imageData, {
+        lang: 'eng',
+        tessedit_char_whitelist: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,!?@#$%&*()-+=/:;"\''
+    });
 
-processedImage.src = canvas.toDataURL();
-rawResult.textContent = result.text;
-tableResult.innerHTML = getTableHTML(result.words);
+    processedImage.src = canvas.toDataURL();
+    rawResult.textContent = result.text;
+    tableResult.innerHTML = getTableHTML(result.words);
 
-requestAnimationFrame(captureVideoFrame);
+    requestAnimationFrame(captureVideoFrame);
 }
 
 // Function to get HTML for table-like result
 function getTableHTML(words) {
-let tableHTML = '<tr><th>Text</th><th>Confidence</th></tr>';
-words.forEach(word => {
-if (word.confidence > 60) {
-tableHTML += <tr><td>${word.text}</td><td>${word.confidence}</td></tr>;
-}
-});
-return tableHTML;
+    let tableHTML = '<tr><th>Text</th><th>Confidence</th></tr>';
+    words.forEach(word => {
+        if (word.confidence > 60) {
+            tableHTML += `<tr><td>${word.text}</td><td>${word.confidence}</td></tr>`;
+        }
+    });
+    return tableHTML;
 }
 
 // Function to check camera access
 function checkCameraAccess() {
-return new Promise((resolve, reject) => {
-navigator.mediaDevices.getUserMedia({ video: true })
-.then((stream) => {
-video.srcObject = stream;
-video.onloadedmetadata = () => {
-video.play();
-resolve();
-};
-})
-.catch((error) => {
-reject(error);
-});
-});
+    return new Promise((resolve, reject) => {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then((stream) => {
+                video.srcObject = stream;
+                video.onloadedmetadata = () => {
+                    video.play();
+                    resolve();
+                };
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
-
