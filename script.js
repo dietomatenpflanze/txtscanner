9a2048a312cc88fcaddfc7b69f430882dd74f782
dietomatenpflanze <130071775+dietomatenpflanze@users.noreply.github.com>
2023-04-07 17:15:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,88 @@
 // Wait for DOM to load
 document.addEventListener("DOMContentLoaded", function () {
-    // Get elements
-    var startButton = document.getElementById("startButton");
-    var stopButton = document.getElementById("stopButton");
-    var resultElement = document.getElementById("result");
-    var feedbackElement = document.getElementById("feedback");
-    var cameraInput = document.getElementById("cameraInput");
-    var canvasOutput = document.getElementById("canvasOutput");
-    var ctx = canvasOutput.getContext("2d");
+// Get elements
+var startButton = document.getElementById("startButton");
+var stopButton = document.getElementById("stopButton");
+var resultElement = document.getElementById("result");
+var feedbackElement = document.getElementById("feedback");
+var cameraInput = document.getElementById("cameraInput");
+var canvasOutput = document.getElementById("canvasOutput");
+var ctx = canvasOutput.getContext("2d");
+var isCameraStarted = false;
+var isTesseractLoaded = false;
 
-    var isCameraStarted = false;
-    var isTesseractLoaded = false;
+// Check for camera compatibility and permissions
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(function (stream) {
+            cameraInput.srcObject = stream;
+            cameraInput.play();
+            startButton.disabled = false;
+        })
+        .catch(function (error) {
+            feedbackElement.textContent =
+                "Camera access denied. Please enable camera permissions.";
+        });
+} else {
+    feedbackElement.textContent = "Camera not supported on this browser.";
+}
 
-    // Check for camera compatibility and permissions
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then(function (stream) {
-                cameraInput.srcObject = stream;
-                cameraInput.play();
-                startButton.disabled = false;
-            })
-            .catch(function (error) {
-                feedbackElement.textContent =
-                    "Camera access denied. Please enable camera permissions.";
-            });
+// Load Tesseract.js library
+Tesseract.initialize({
+    lang: "eng",
+    workerPath: "https://cdn.jsdelivr.net/npm/tesseract.js@2.1.4/dist/worker.min.js",
+    corePath: "https://cdn.jsdelivr.net/npm/tesseract.js@2.1.4/dist/tesseract-core.wasm.js",
+})
+.then(function () {
+    isTesseractLoaded = true;
+    feedbackElement.textContent = "Tesseract.js loaded successfully.";
+    // Show success dialog
+    showDialog('Success', 'Tesseract.js library loaded successfully!');
+})
+.catch(function (error) {
+    feedbackElement.textContent = "Failed to load Tesseract.js.";
+    // Show error dialog
+    showDialog('Error', 'Failed to load Tesseract.js library. Error: ' + error.message);
+});
+
+// Start button click event
+startButton.addEventListener("click", function () {
+    if (isCameraStarted && isTesseractLoaded) {
+        feedbackElement.textContent = "Detecting text...";
+        startButton.disabled = true;
+        stopButton.disabled = false;
+        takeSnapshotAndDetectTextFromCamera();
     } else {
-        feedbackElement.textContent = "Camera not supported on this browser.";
-    }
-
-    // Load Tesseract.js library
-    Tesseract.initialize({
-        lang: "eng",
-        workerPath: "https://cdn.jsdelivr.net/npm/tesseract.js@2.1.4/dist/worker.min.js",
-        corePath: "https://cdn.jsdelivr.net/npm/tesseract.js@2.1.4/dist/tesseract-core.wasm.js",
-    })
-    .then(function () {
-        isTesseractLoaded = true;
-        feedbackElement.textContent = "Tesseract.js loaded successfully.";
-        // Show success dialog
-        showDialog('Success', 'Tesseract.js library loaded successfully!');
-    })
-    .catch(function (error) {
-        feedbackElement.textContent = "Failed to load Tesseract.js.";
-        // Show error dialog
-        showDialog('Error', 'Failed to load Tesseract.js library. Error: ' + error.message);
-    });
-
-    // Start button click event
-    startButton.addEventListener("click", function () {
-        if (isCameraStarted && isTesseractLoaded) {
-            feedbackElement.textContent = "Detecting text...";
-            startButton.disabled = true;
-            stopButton.disabled = false;
-            detectTextFromCamera();
-        } else {
-            if (!isCameraStarted) {
-                feedbackElement.textContent = "Camera not started yet.";
-            }
-            if (!isTesseractLoaded) {
-                feedbackElement.textContent = "Tesseract.js not loaded yet.";
-            }
+        if (!isCameraStarted) {
+            feedbackElement.textContent = "Camera not started yet.";
         }
-    });
+        if (!isTesseractLoaded) {
+            feedbackElement.textContent = "Tesseract.js not loaded yet.";
+        }
+    }
+});
 
-    // Stop button click event
-    stopButton.addEventListener("click", function () {
-        stopButton.disabled = true;
-        startButton.disabled = false;
-        feedbackElement.textContent = "Text detection stopped.";
-    });
-    
-    // Function to show dialog
+// Stop button click event
+stopButton.addEventListener("click", function () {
+    stopButton.disabled = true;
+    startButton.disabled = false;
+    feedbackElement.textContent = "Text detection stopped.";
+});
+
+// Function to show dialog
 function showDialog(title, message) {
     alert(title + ': ' + message);
 }
 
-    // Function to detect text from camera
-    function detectTextFromCamera() {
-        ctx.drawImage(cameraInput, 0, 0, canvasOutput.width, canvasOutput.height);
-        var imageData = ctx.getImageData(0, 0, canvasOutput.width, canvasOutput.height);
+// Function to take snapshot and detect text from camera
+function takeSnapshotAndDetectTextFromCamera() {
+    ctx.drawImage(cameraInput, 0, 0, canvasOutput.width, canvasOutput.height);
+    var imageData = ctx.getImageData(0, 0, canvasOutput.width, canvasOutput.height);
+    var img = new Image();
+    img.src = canvasOutput.toDataURL();
+    img.onload = function() {
+        resultElement.src = img.src;
         Tesseract.recognize(imageData)
         .then(function (result) {
             resultElement.textContent = result.text;
@@ -90,5 +93,6 @@ function showDialog(title, message) {
             stopButton.disabled = true;
             startButton.disabled = false;
         });
-    }
+    };
+}
 });
